226eeee5063ff44037f79159fa5510b4642d13cd
b1acksmyth <smith.spence13@gmail.com>
2020-06-26 15:29:17 -0700

    basic code outline

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", "<", ">", "/"];
+let characters = [charLower, charUpper, number, special];
+ // let validLength = (charCount < 8 || charCount > 128);
 
+ 
+function generatePassword(){
+  let charCount = parseInt(prompt("Length must be 8 to 128 characters long. How long do you want the password length to be?"));
+  let lower = confirm("Do you want to add lowercase letters?");
+  let upper  = confirm("Do you want to add uppercase letters?");
+  let numbers = confirm("Do you want to add numbers?");
+  let special = confirm("Do you want to add special characters?");
+
+  if (lower === true){
+    
+  }
+
+  if (upper === true){
+    
+  }
+
+  if (numbers === true){
+    
+  }
+
+  if (special === true){
+    
+  }
+  
+  if (charCount >= 8 && charCount <= 128){
+    Math.floor(Math.random());
+  }
+}
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
