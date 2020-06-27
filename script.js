3ecd97a3a2f6f82cbdeba3148ab4de0800a6ee2f
b1acksmyth <smith.spence13@gmail.com>
2020-06-26 19:41:09 -0700

    finished code to get password to generate

--- script.js
+++ script.js
@@ -7,33 +7,42 @@ let special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", 
 let characters = [charLower, charUpper, number, special];
  // let validLength = (charCount < 8 || charCount > 128);
 
- 
+
 function generatePassword(){
   let charCount = parseInt(prompt("Length must be 8 to 128 characters long. How long do you want the password length to be?"));
   let lower = confirm("Do you want to add lowercase letters?");
   let upper  = confirm("Do you want to add uppercase letters?");
   let numbers = confirm("Do you want to add numbers?");
   let special = confirm("Do you want to add special characters?");
+  let options = [];
+  let password = "";
 
   if (lower === true){
-    
+    options.push(0)
   }
 
   if (upper === true){
-    
+    options.push(1)
   }
 
   if (numbers === true){
-    
+    options.push(2)
   }
 
   if (special === true){
-    
+    options.push(3)
   }
   
   if (charCount >= 8 && charCount <= 128){
-    Math.floor(Math.random());
+    for (let i = 0; i < charCount; i++){
+      let charType = options[Math.floor(Math.random() * options.length)];
+      let charRandom = characters[charType];
+      password += charRandom[Math.floor(Math.random() * charRandom.length)];
+      //console.log("passwrodLoop: ", password);
+    }
   }
+  //console.log("password: ", password)
+  return password
 }
 // Write password to the #password input
 function writePassword() {
